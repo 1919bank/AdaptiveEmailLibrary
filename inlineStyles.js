308969ee2e494ab13b6a1a590c23e0a6051831fd
1919bank <cesar.a.reyes@outlook.com>
2024-11-05 14:26:59 -0800

    Added inline styling

--- inlineStyles.js
+++ inlineStyles.js
@@ -1,21 +1,45 @@
 const fs = require('fs');
+const path = require('path');
 const juice = require('juice');
 
-const folderPath = './build';
-fs.readdir(folderPath, (err, files) => {
-    if (err) {
-        console.error('Could not list the directory.', err);
-        process.exit(1);
-    }
+const buildDir = path.join(__dirname, 'build');
 
-    files.forEach((file) => {
-        if (file.endsWith('.html')) {
-            const filePath = `${folderPath}/${file}`;
-            const htmlContent = fs.readFileSync(filePath, 'utf8');
-            const inlinedHtml = juice(htmlContent);
-
-            fs.writeFileSync(filePath, inlinedHtml, 'utf8');
-            console.log(`Styles inlined for ${file}`);
+// Function to inline CSS for each HTML file in the build directory
+function inlineStyles() {
+    fs.readdir(buildDir, (err, files) => {
+        if (err) {
+            console.error('Error reading build directory:', err);
+            return;
         }
+
+        // Filter for HTML files
+        const htmlFiles = files.filter(file => file.endsWith('.html'));
+
+        htmlFiles.forEach(file => {
+            const filePath = path.join(buildDir, file);
+
+            // Read the HTML file content
+            fs.readFile(filePath, 'utf-8', (err, data) => {
+                if (err) {
+                    console.error(`Error reading file ${file}:`, err);
+                    return;
+                }
+
+                // Inline styles using juice
+                const inlinedHtml = juice(data);
+
+                // Write the inlined HTML back to the file
+                fs.writeFile(filePath, inlinedHtml, 'utf-8', err => {
+                    if (err) {
+                        console.error(`Error writing file ${file}:`, err);
+                    } else {
+                        console.log(`Successfully inlined styles for ${file}`);
+                    }
+                });
+            });
+        });
     });
-});
+}
+
+// Run the function
+inlineStyles();
